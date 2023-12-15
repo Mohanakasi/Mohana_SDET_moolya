a016b37e0d6393469c87b076e992ecda71c64bdb
mohanakasi.s3@gmail.com <mohanakasi.s3@gmail.com>
2023-12-15 12:17:24 +0530

    updating json file path

--- Python_API_frame_work_DD/tests/test_simple_book_requests.py
+++ Python_API_frame_work_DD/tests/test_simple_book_requests.py
@@ -4,8 +4,8 @@ from Python_API_frame_work_DD.request_methods.api_methods import Rest_api_method
 from random import randint
 class Test_Simple_books_class(Rest_api_methods):
     Rest_api_methods.base_url = r'https://simple-books-api.glitch.me'
-    Rest_api_methods.END_payload_file = r"../json_files/pay_load_file.json"
-    Rest_api_methods.test_data_file_path = r"../json_files/test_data.json"
+    Rest_api_methods.END_payload_file = r"..\\Python_API_frame_work_DD\json_files\pay_load_file.json"
+    Rest_api_methods.test_data_file_path = r"..\\Python_API_frame_work_DD\json_files\test_data.json"
 
 
     def test_getting_access_token(self):
